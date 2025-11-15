1c459bd8d30af3560a984b4f82fe98f566e2e407
p-sofii33 <115869778+p-sofii33@users.noreply.github.com>
2025-11-15 12:47:25 +0200

    controller

--- controller/controller.ino
+++ controller/controller.ino
@@ -1,10 +1,8 @@
 int buttonPin1 = 7, buttonPin2 = 8, buttonPin3 = 6;
 
-// Current and previous STABLE states
 int buttonState1 = LOW, buttonState2 = LOW, buttonState3 = LOW;
 int lastReading1 = LOW, lastReading2 = LOW, lastReading3 = LOW;
 
-// Debounce timers
 unsigned long lastDebounceTime1 = 0;
 unsigned long lastDebounceTime2 = 0;
 unsigned long lastDebounceTime3 = 0;
@@ -19,31 +17,20 @@ void setup() {
   Serial.begin(9600);
 }
 
-// Generic debounced press check
-void checkButton(int pin, int care,
-                 int &lastReading,
-                 int &buttonState,
-                 unsigned long &lastTime)
+void checkButton(int pin, int care, int &lastReading, int &buttonState, unsigned long &lastTime)
 {
   int reading = digitalRead(pin);
-
-  // If reading changed, reset the debounce timer
   if (reading != lastReading) {
     lastTime = millis();
   }
-
-  // If stable for longer than debounceDelay, accept it
   if ((millis() - lastTime) > debounceDelay) {
     if (reading != buttonState) {
       buttonState = reading;
-
-      // Button pressed (LOW->HIGH)
       if (buttonState == HIGH) {
-        Serial.println("Button " + (String)care + " pressed");
+        Serial.println("Button " + (String)care + " pressed\n");
       }
     }
   }
-
   lastReading = reading;
 }
 
