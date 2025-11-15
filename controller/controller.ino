693473a947b7b338f41d56f96eae7ca5e579a995
p-sofii33 <115869778+p-sofii33@users.noreply.github.com>
2025-11-15 22:08:03 +0200

    quick fix

--- controller/controller.ino
+++ controller/controller.ino
@@ -1,11 +1,12 @@
-int buttonPin1 = 7, buttonPin2 = 8, buttonPin3 = 6;
+int buttonPin1 = 2, buttonPin2 = 3, buttonPin3 = 4, buttonPin4 = 5;
 
-int buttonState1 = LOW, buttonState2 = LOW, buttonState3 = LOW;
-int lastReading1 = LOW, lastReading2 = LOW, lastReading3 = LOW;
+int buttonState1 = LOW, buttonState2 = LOW, buttonState3 = LOW, buttonState4 = LOW;
+int lastReading1 = LOW, lastReading2 = LOW, lastReading3 = LOW, lastReading4 = LOW;
 
 unsigned long lastDebounceTime1 = 0;
 unsigned long lastDebounceTime2 = 0;
 unsigned long lastDebounceTime3 = 0;
+unsigned long lastDebounceTime4 = 0;
 
 unsigned long debounceDelay = 50;
 
@@ -13,7 +14,7 @@ void setup() {
   pinMode(buttonPin1, INPUT);
   pinMode(buttonPin2, INPUT);
   pinMode(buttonPin3, INPUT);
-
+  pinMode(buttonPin4, INPUT);
   Serial.begin(9600);
 }
 
@@ -38,4 +39,5 @@ void loop() {
   checkButton(buttonPin1, 1, lastReading1, buttonState1, lastDebounceTime1);
   checkButton(buttonPin2, 2, lastReading2, buttonState2, lastDebounceTime2);
   checkButton(buttonPin3, 3, lastReading3, buttonState3, lastDebounceTime3);
+  checkButton(buttonPin4, 4, lastReading4, buttonState4, lastDebounceTime4);
 }
